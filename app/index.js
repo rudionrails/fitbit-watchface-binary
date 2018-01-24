982604492a91724f416c78281b22f67b2c69cb5a
Rudolf Schmidt <me@rudionrails.com>
2018-01-24 17:40:03 +0100

    Fixes linter offenses

--- app/index.js
+++ app/index.js
@@ -1,49 +1,49 @@
-import clock from "clock";
-import document from "document";
+import clock from 'clock';
+import document from 'document';
 
-import { display } from "display";
-import { peerSocket } from "messaging";
+import { display } from 'display';
+import { peerSocket } from 'messaging';
 
 import { colors } from '../common/colors';
 import { fillDOMNodes } from '../common/utils';
 
 // screen
-const screenDOM     = document.getElementById('screen');
+const screenDOM = document.getElementById('screen');
 const backgroundDOM = document.getElementById('background');
 
 // binary clock elements
-const hoursLeftDOM    = document.getElementsByClassName('Binary-hours-left');
-const hoursRightDOM   = document.getElementsByClassName('Binary-hours-right');
-const minutesLeftDOM  = document.getElementsByClassName('Binary-minutes-left');
+const hoursLeftDOM = document.getElementsByClassName('Binary-hours-left');
+const hoursRightDOM = document.getElementsByClassName('Binary-hours-right');
+const minutesLeftDOM = document.getElementsByClassName('Binary-minutes-left');
 const minutesRightDOM = document.getElementsByClassName('Binary-minutes-right');
-const secondsLeftDOM  = document.getElementsByClassName('Binary-seconds-left');
+const secondsLeftDOM = document.getElementsByClassName('Binary-seconds-left');
 const secondsRightDOM = document.getElementsByClassName('Binary-seconds-right');
 
 // digital clock elements
-const digitalDOM            = document.getElementById("digital");
-const digitalTimeDOM        = document.getElementById("digital-time");
-const digitalTimeShadowDOM  = document.getElementById("digital-shadow");
+const digitalDOM = document.getElementById('digital');
+const digitalTimeDOM = document.getElementById('digital-time');
+const digitalTimeShadowDOM = document.getElementById('digital-shadow');
 
 let themeColor = colors[0].color;
 
 function set(key, value) {
   switch (key) {
-    case "isDisplayAlwaysOn":
+    case 'isDisplayAlwaysOn':
       display.autoOff = value !== true;
       break;
-    case "isDigitalClockDisabled":
+    case 'isDigitalClockDisabled':
       digitalDOM.style.display = (value === true) ? 'none' : 'inherit';
       break;
-    case "themeColor":
+    case 'themeColor':
       themeColor = value;
       break;
   }
 }
 
 function updateClock({ date }) {
-  const seconds = ('0' + date.getSeconds()).slice(-2);
-  const minutes = ('0' + date.getMinutes()).slice(-2);
-  const hours   = ('0' + date.getHours()).slice(-2);
+  const seconds = (`0${date.getSeconds()}`).slice(-2);
+  const minutes = (`0${date.getMinutes()}`).slice(-2);
+  const hours = (`0${date.getHours()}`).slice(-2);
   const time = [hours, minutes, seconds].join(':');
 
   try {
@@ -62,21 +62,21 @@ function updateClock({ date }) {
 /**
 * Clock
 */
-clock.granularity = "seconds";
+clock.granularity = 'seconds';
 clock.ontick = updateClock;
 
 /**
 * Messaging
 */
-peerSocket.onopen = () => console.log("[App] Socket open");
-peerSocket.onclose = () => console.log("[App] Socket closed");
+peerSocket.onopen = () => console.log('[App] Socket open');
+peerSocket.onclose = () => console.log('[App] Socket closed');
 peerSocket.onmessage = ({ data }) => {
   display.on = true; // switch on the display for changes in the settings
 
   try {
     console.log(`[App] received ${JSON.stringify(data)}`);
     set(data.key, data.newValue);
-  } catch(e) {
-    console.error(`[App] Could not set ${JSON.stringify(data)}`)
+  } catch (e) {
+    console.error(`[App] Could not set ${JSON.stringify(data)}`);
   }
 };
--- common/utils.js
+++ common/utils.js
@@ -1,16 +1,16 @@
-const toBinary = (num) => ('0000' + (num >>> 0).toString(2)).slice(-4);
+const toBinary = num => (`0000${(num >>> 0).toString(2)}`).slice(-4);
 
 function apply(color, value, nodes) {
   const digits = toBinary(value).split('').reverse();
- 
+
   nodes.forEach((node, index) => {
     node.style.fill = (digits[index] === '1') ? color : 'gray';
   });
 }
 
 export function fillDOMNodes(color, number, leftDOM, rightDOM) {
-  const [ leftBinary, rightBinary ] = `${number}`.split('');
-  
+  const [leftBinary, rightBinary] = `${number}`.split('');
+
   apply(color, leftBinary, leftDOM);
   apply(color, rightBinary, rightDOM);
 }
